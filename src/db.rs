045d0913da43314156a2540e2a3faeb3692500ba
DanielMehlber <daniel.mehlber@gmail.com>
2021-10-04 09:45:10 +0200

    sql url conactted connection established

--- src/db.rs
+++ src/db.rs
@@ -1,7 +1,7 @@
 use diesel::{Connection, mysql};
 
-pub fn establish_connection() -> mysql::MysqlConnection {
-    let connection = mysql::MysqlConnection::establish("mysql://localhost:3306/Home?user=readuser").unwrap();
+pub fn establish_connection<'method>(host: &'method str, port: u16, database: &'method str,user: &'method str, password: &'method str) -> mysql::MysqlConnection {
+    let connection = mysql::MysqlConnection::establish(format!("mysql://{}:{}@{}:{}/{}", user, password, host, port, database).as_str()).unwrap();
 
     connection
 }
