209345faeb9810150cd776076109d357a5468b05
DanielMehlber <daniel.mehlber@gmail.com>
2021-10-06 18:56:19 +0200

    documentation added

--- src/main.rs
+++ src/main.rs
@@ -5,13 +5,18 @@ use actix_web::{App, HttpResponse, HttpServer, get, web};
 use diesel::{MysqlConnection, QueryDsl, RunQueryDsl, r2d2::{ConnectionManager, Pool}};
 use person::Person;
 
+// imports important traits (they must be in scope in order to work)
 use self::diesel::prelude::*;
 
 mod person;
 mod db;
 mod schema;
 
+/**
+ * Actix-Web Application data passed into API functions via web::Data<MyAppData> parameter
+ */
 struct MyAppData {
+    // connection pool instead of raw-connection
     pub pool: Pool<ConnectionManager<MysqlConnection>>
 }
 
@@ -24,16 +29,22 @@ async fn index() -> &'static str {
 }
 
 /**
- * retreive some person data (automatically serialized into JSON)
+ * create some person data and send it in JSON format
  */
 #[get("/person")]
 async fn get_person() -> person::Person {
     person::Person::new(String::from("Daniel Mehlber"), String::from("This is a test program"), 19)
 }
 
+/**
+ * search for user in database
+ */
 #[get("/search/{name}")]
 async fn search_person(web::Path(search): web::Path<String>, data: web::Data<MyAppData>) -> HttpResponse {
+    //                 ^^^^^^^^^^^^^^^^^ path params         ^^^^ application data set in setup
     let connection = data.pool.get().unwrap();
+
+    // *::dsl contains helper types and bare functions not contained in prelude
     use schema::person::dsl::{name, person};
 
     let people = person.filter(name.like(format!("%{}%", search))).load::<Person>(&connection);
@@ -46,6 +57,7 @@ async fn search_person(web::Path(search): web::Path<String>, data: web::Data<MyA
         Err(err) => HttpResponse::InternalServerError().body(err.to_string())
     }
 }
+
 /*
  * Run server and mount API
  */
@@ -60,7 +72,9 @@ async fn main() -> std::io::Result<()> {
 
 
     HttpServer::new(move || { 
-        App::new().service(index).service(get_person).service(search_person)
-        .data(MyAppData {pool: pool.clone()})
+        App::new()  .service(index)
+                    .service(get_person)
+                    .service(search_person)
+                    .data(MyAppData {pool: pool.clone()}) // set application data (used in API functions)
     }).bind("localhost:8080")?.run().await
 }
